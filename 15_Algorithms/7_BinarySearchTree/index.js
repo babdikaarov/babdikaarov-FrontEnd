4cba935cc844f51550d21fed139ad511a8e2168b
Macbook Beks <babdikaarov@gmail.com>
2023-09-25 19:01:46 +0300

    codecademy progress

---
+++ 15_Algorithms/7_BinarySearchTree/index.js
@@ -0,0 +1,81 @@
+class BinaryTree {
+  constructor(value, depth = 1) {
+    this.value = value;
+    this.depth = depth;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    if (value < this.value) {
+      if (!this.left) {
+        this.left = new BinaryTree(value, this.depth + 1);
+      } else {
+        this.left.insert(value);
+      }
+    } else {
+      if (!this.right) {
+        this.right = new BinaryTree(value, this.depth + 1);
+      } else {
+        this.right.insert(value);
+      }
+    }
+  }
+
+  getNodeByValue(value) {
+    if (this.value === value) {
+      return this;
+    } else if (this.left && value < this.value) {
+      return this.left.getNodeByValue(value);
+    } else if (this.right) {
+      return this.right.getNodeByValue(value);
+    } else {
+      return null;
+    }
+  }
+
+  depthFirstTraversal() {
+    if (this.left) {
+      this.left.depthFirstTraversal();
+    }
+    switch (this.depth) {
+      case 1:
+        console.log("------" + this.value);
+        break;
+      case 2:
+        console.log("-----" + this.value);
+        break;
+      case 3:
+        console.log("----" + this.value);
+        break;
+      case 4:
+        console.log("---" + this.value);
+        break;
+      case 5:
+        console.log("--" + this.value);
+        break;
+      case 6:
+        console.log("-" + this.value);
+        break;
+    }
+    // console.log(`Depth=${this.depth}, Value=${this.value}`);
+    if (this.right) {
+      this.right.depthFirstTraversal();
+    }
+  }
+}
+
+// create a BinaryTree here
+const randomize = () => Math.floor(Math.random() * 40);
+const bt = new BinaryTree(15);
+let numbers = [];
+
+for (let i = 0; i < 10; i++) {
+  numbers.push(randomize());
+  bt.insert(numbers[i]);
+}
+
+console.log(`Inserted [ ${numbers} ] to binary tree`);
+
+console.log("Depth First Traversal");
+bt.depthFirstTraversal();
